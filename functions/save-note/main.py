72a8c5ae8c6aec6af8884c1d411377c8fa09811e
LionelHasan <lionelhasan52@gmail.com>
2023-03-21 10:11:59 -0600

    Added TF

--- functions/save-note/main.py
+++ functions/save-note/main.py
@@ -1,1 +1,31 @@
-# add your save-note function here
+import json
+import boto3
+
+
+dynamodb_resource = boto3.resource("dynamodb")
+table = dynamodb_resource.Table("notes") 
+
+def savenote(event, context):
+    item = {
+    "noteid": 3098390,
+    }
+        # Setting the variable body to the json body attribute (sent by the endpoint via LambdaURL), json loads converts it to a python dictionary
+    body = json.loads(event["body"])
+    try:
+        table.put_item(Item=body)
+    except Exception as exp:
+        print("exception: {exp}") # Can send the exception to Cloudwatch
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": str(exp)
+            })
+        }
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "success"
+        })
+
+    }
