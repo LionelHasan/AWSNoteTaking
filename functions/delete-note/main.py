d7fd127bcc4d5659c91a7c757e14028eda498e68
LionelHasan <lionelhasan52@gmail.com>
2023-03-21 18:18:00 -0600

    Created lamba function URLS for all funcs

--- functions/delete-note/main.py
+++ functions/delete-note/main.py
@@ -1,1 +1,33 @@
-# add your delete-note function here
+import json
+import boto3
+
+
+dynamodb_resource = boto3.resource("dynamodb")
+table = dynamodb_resource.Table("notes") 
+
+def lambda_handler(event, context):
+
+        # Setting the variable body to the json body attribute (sent by the endpoint via LambdaURL), json loads converts it to a python dictionary
+    body = json.loads(event["body"])
+    
+
+    try:
+        table.delete_item(Item=body.get("id"))
+    except Exception as exp:
+        print("exception: {exp}") # Can send the exception to Cloudwatch
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": str(exp)
+            })
+        }
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "success"
+        })
+
+    }
+
+    
--- infra/main.tf
+++ infra/main.tf
@@ -16,6 +16,8 @@ provider "aws" {
 # you can use throughout your code
 locals {
   function_name = "save-note30137476"
+  delete_note = "delete-note30137476"
+  get_note = "get-note30137476"
   handler_name  = "main.lambda_handler"
   artifact_name = "artifact.zip"
 }
@@ -93,9 +95,169 @@ resource "aws_iam_policy" "logs" {
 EOF
 }
 
+
+
 # attach the above policy to the function role
 # see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = aws_iam_policy.logs.arn
+}
+
+resource "aws_iam_role" "delete-lambda" {
+  name               = "iam-for-lambda-${local.delete_note}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# create archive file from main.py
+data "archive_file" "delete-lambda" {
+  type = "zip"
+  # this file (main.py) needs to exist in the same folder as this 
+  # Terraform configuration file
+  source_file = "../functions/delete-note/main.py"
+  output_path = "artifact.zip"
+}
+
+# create a Lambda function
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
+resource "aws_lambda_function" "delete-lambda" {
+  role             = aws_iam_role.lambda.arn
+  function_name    = local.delete_note
+  handler          = local.handler_name
+  filename         = local.artifact_name
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
+  runtime = "python3.9"
+}
+
+# create a policy for publishing logs to CloudWatch
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+resource "aws_iam_policy" "delete-logs" {
+  name        = "lambda-logging-${local.delete_note}"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "dynamodb:*"
+
+
+      ],
+      "Resource": ["arn:aws:logs:*:*:*", "arn:aws:dynamodb:*:*:table/*", "arn:aws:dynamodb:*:*:*/*"],
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+
+
+# attach the above policy to the function role
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "delete_logs" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = aws_iam_policy.logs.arn
+}
+
+
+
+
+
+resource "aws_iam_role" "get-lambda" {
+  name               = "iam-for-lambda-${local.get_note}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# create archive file from main.py
+data "archive_file" "get-lambda" {
+  type = "zip"
+  # this file (main.py) needs to exist in the same folder as this 
+  # Terraform configuration file
+  source_file = "../functions/get-notes/main.py"
+  output_path = "artifact.zip"
+}
+
+# create a Lambda function
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
+resource "aws_lambda_function" "get-lambda" {
+  role             = aws_iam_role.lambda.arn
+  function_name    = local.get_note
+  handler          = local.handler_name
+  filename         = local.artifact_name
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
+  runtime = "python3.9"
+}
+
+# create a policy for publishing logs to CloudWatch
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+resource "aws_iam_policy" "get-logs" {
+  name        = "lambda-logging-${local.get_note}"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "dynamodb:*"
+
+
+      ],
+      "Resource": ["arn:aws:logs:*:*:*", "arn:aws:dynamodb:*:*:table/*", "arn:aws:dynamodb:*:*:*/*"],
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+
+
+# attach the above policy to the function role
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "get_logs" {
   role       = aws_iam_role.lambda.name
   policy_arn = aws_iam_policy.logs.arn
 }
@@ -116,6 +278,55 @@ resource "aws_lambda_function_url" "url" {
   }
 }
 
+# show the Function URL after creation
+output "lambda_url" {
+  value = aws_lambda_function_url.url.function_url
+}
+
+
+
+resource "aws_lambda_function_url" "delete_url" {
+  function_name      = local.delete_note
+  authorization_type = "NONE"
+
+  cors {
+    
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET", "POST", "PUT", "DELETE"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+# show the Function URL after creation
+output "delete-url" {
+  value = aws_lambda_function_url.delete_url.function_url
+}
+
+resource "aws_lambda_function_url" "get_url" {
+  function_name      = local.get_note
+  authorization_type = "NONE"
+
+  cors {
+    
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET", "POST", "PUT", "DELETE"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+# show the Function URL after creation
+output "get-url" {
+  value = aws_lambda_function_url.get_url.function_url
+}
+
+
+
+
+
 resource "aws_dynamodb_table" "notes" {
   name         = "notes"
   billing_mode = "PROVISIONED"
@@ -134,8 +345,3 @@ attribute {
 }
 
 
-
-# show the Function URL after creation
-output "lambda_url" {
-  value = aws_lambda_function_url.url.function_url
-}
